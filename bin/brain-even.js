9a9857c8e62a6f20ca96ccdc3863eff40c314111
Beleaev Daniil <daniil.beleaev@mail.ru>
2025-02-25 15:31:42 +0300

    fix brain-even game

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -5,25 +5,32 @@ import readlineSync from 'readline-sync';
 
 const playGame = (name) => {
     console.log(`Welcome, to the Brain Games!`);
-    const userName = greet();
+    console.log('Answer "yes" if he number is even, otherwize answer "no".')
+
     let correctAnswers = 0;
+
     while (correctAnswers < 3) {
-      const number = Math.floor(Math.random() * 100) + 1;
+      const number = Math.floor(Math.random() * 100);
       const answer = readlineSync.question(`Question: ${number}\nYour answer: `);
+      
       if (answer.toLowerCase() !== "yes" && answer.toLowerCase() !== "no") {
         console.log(`'${answer}' is wrong answer ;(. Correct answer was '${number % 2 === 0 ? "yes" : "no"}'.`);
         console.log(`Let's try again, ${name}!`);
         return;
+
       } else if ((number % 2 === 0 && answer.toLowerCase() === "yes") || (number % 2 !== 0 && answer.toLowerCase() === "no")) {
         console.log("Correct!");
-        correctAnswers++;
+        correctAnswers+= 1;
+        
       } else {
         console.log(`'${answer}' is wrong answer ;(. Correct answer was '${number % 2 === 0 ? "yes" : "no"}'.`);
         console.log(`Let's try again, ${name}!`);
         return;
       }
     }
+
     console.log(`Congratulations, ${name}!`);
 };
 
-playGame();
+const userName = greet();
+playGame(userName);
