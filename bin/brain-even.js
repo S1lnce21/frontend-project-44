1709b213a2b3c97624df413a1d98e0dd471c7b84
Beleaev Daniil <daniil.beleaev@mail.ru>
2025-02-28 09:11:15 +0300

    fix brain-even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,27 +1,22 @@
 #!/usr/bin/env node
 
-import greet from '../src/cli.js';
 import readlineSync from 'readline-sync';
 
+const greet = () => {
+    console.log('Welcome to the Brain Games!');
+    const name = readlineSync.question('May I have your name? ');
+    return name;
+};
+
 const playGame = (name) => {
-    console.log(`Welcome, to the Brain Games!`);
-    console.log('Answer "yes" if he number is even, otherwize answer "no".')
+    console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
-    let correctAnswers = 0;
-
-    while (correctAnswers < 3) {
+    for (let i = 0; i < 3; i++) {
       const number = Math.floor(Math.random() * 100);
       const answer = readlineSync.question(`Question: ${number}\nYour answer: `);
-      
-      if (answer.toLowerCase() !== "yes" && answer.toLowerCase() !== "no") {
-        console.log(`'${answer}' is wrong answer ;(. Correct answer was '${number % 2 === 0 ? "yes" : "no"}'.`);
-        console.log(`Let's try again, ${name}!`);
-        return;
 
-      } else if ((number % 2 === 0 && answer.toLowerCase() === "yes") || (number % 2 !== 0 && answer.toLowerCase() === "no")) {
+      if ((number % 2 === 0 && answer.toLowerCase() === "yes") || (number % 2 !== 0 && answer.toLowerCase() === "no")) {
         console.log("Correct!");
-        correctAnswers+= 1;
-        
       } else {
         console.log(`'${answer}' is wrong answer ;(. Correct answer was '${number % 2 === 0 ? "yes" : "no"}'.`);
         console.log(`Let's try again, ${name}!`);
